1cce36080f9a352453cd08c16f81f3adbfaa33fb
Caroline Sampaio <caroline_08022@hotmail.com>
2024-08-21 20:17:46 -0300

    chore: remove comentario api rotas autenticadas

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,3 @@ use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('alunos', StudentController::class);
-
-
-// Rotas protegidas por autenticação
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
