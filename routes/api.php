c4823ea4aff080a3ba9ca53e6989e01e366eb55b
Caroline Sampaio <caroline_08022@hotmail.com>
2024-08-21 17:01:54 -0300

    feat: adiciona base projeto laravel

---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Rotas protegidas por autenticação
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
