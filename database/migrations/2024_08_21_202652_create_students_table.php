839057271b3710bb80e8f0f41f6fe6ed604fb5ff
Caroline Sampaio <caroline_08022@hotmail.com>
2024-08-21 17:41:55 -0300

    feat: adiciona tabela alunos db

---
+++ database/migrations/2024_08_21_202652_create_students_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('alunos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 100);
+            $table->integer('idade');
+            $table->decimal('nota_primeiro_semestre', 5, 2);
+            $table->decimal('nota_segundo_semestre', 5, 2);
+            $table->string('nome_professor', 100);
+            $table->integer('numero_sala');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('alunos');
+    }
+};
